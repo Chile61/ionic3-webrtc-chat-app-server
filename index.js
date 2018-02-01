9b6dc7cbd68d0eaa24e5187e5702070d6458f4da
Kross <krossevruiz@gmail.com>
2018-02-01 12:28:58 -0800

    Changed naming convention a smidge, now going to add signaling events

--- index.js
+++ index.js
@@ -6,12 +6,33 @@ const https = require('https');
 const SERVER_PORT_NUMBER = 8000;
 const server = http.createServer().listen(SERVER_PORT_NUMBER, '0.0.0.0');
 const io = socketio.listen(server);
+console.log("Listening on port", SERVER_PORT_NUMBER);
 
 io.sockets.on('connection', client => {
-	console.log(Object.keys(io.sockets.sockets));
+	let originalId = client.id;
+	// console.log(Object.keys(io.sockets.sockets));
 	//client.emit('omae_wa_mou_shindeiru');
-	//client.on('example', () => {
-	//	client.emit('nane', {});
-	//	client.broadcast.emit('other_name', {});
-	//});
+	client.on('updated-display-name', (newDisplayName) => {
+		// client.emit('nane', {});
+		// client.broadcast.emit('other_name', {});
+
+		io.sockets.sockets[client.id].name = newDisplayName;
+
+		console.log("ACTIVE USERS");
+		for(let sock in io.sockets.sockets){
+			console.log(io.sockets.sockets[sock].name);
+		}
+	});
+
+	client.on('disconnect', (a) => {
+		console.log("A CLIENT DISCONNECTED B");
+		console.log(`${client.id} disconnected`);
+		console.log(`${a} disconnected`);
+		console.log("A CLIENT DISCONNECTED TERM");
+		// console.log(`${io.sockets.sockets[client.id].name} disconnected`);
+		// for(let sock in io.sockets.sockets){
+		// 	console.log(io.sockets.sockets[sock].name);
+		// }
+	});
+
 });
